eba839d6bda3fd577599926fe66d21c05d802a6e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-12-06 10:30:39 +0100

    fix: use non-capturing group to wrap multicharacter patterns

--- src/__tests__/quantifiers.test.tsx
+++ src/__tests__/quantifiers.test.tsx
@@ -1,9 +1,9 @@
 import { one, oneOrMore, optionally, zeroOrMore } from '../quantifiers';
-import { buildPattern } from '../compiler';
+import { buildPattern, buildRegex } from '../compiler';
 
 test('"oneOrMore" quantifier', () => {
   expect(buildPattern(oneOrMore('a'))).toEqual('a+');
-  expect(buildPattern(oneOrMore('ab'))).toEqual('(ab)+');
+  expect(buildPattern(oneOrMore('ab'))).toEqual('(?:ab)+');
 });
 
 test('"one" quantifier', () => {
@@ -13,10 +13,16 @@ test('"one" quantifier', () => {
 
 test('"optionally" quantifier', () => {
   expect(buildPattern(optionally('a'))).toEqual('a?');
-  expect(buildPattern(optionally('ab'))).toEqual('(ab)?');
+  expect(buildPattern(optionally('ab'))).toEqual('(?:ab)?');
 });
 
 test('"zeroOrMore" quantifier', () => {
   expect(buildPattern(zeroOrMore('a'))).toEqual('a*');
-  expect(buildPattern(zeroOrMore('ab'))).toEqual('(ab)*');
+  expect(buildPattern(zeroOrMore('ab'))).toEqual('(?:ab)*');
+});
+
+test('oneOrMore does not generate capture when grouping', () => {
+  const regex = buildRegex(oneOrMore('aa'));
+  const groups = [...'aa'.match(regex)!];
+  expect(groups).toEqual(['aa']);
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
-export function wrapGroup(input: string): string {
-  return input.length === 1 ? input : `(${input})`;
+/**
+ * Wraps regex string in a non-capturing group if it is more than one character long.
+ *
+ * @param regex
+ * @returns
+ */
+export function wrapGroup(regex: string): string {
+  return regex.length === 1 ? regex : `(?:${regex})`;
 }
