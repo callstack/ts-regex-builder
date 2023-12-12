d9212a63651208aeb02095c8f2453c1019a4d826
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-12-12 18:22:38 +0100

    chore: fix typos

--- src/character-classes/compiler.ts
+++ src/character-classes/compiler.ts
@@ -9,13 +9,13 @@ export function compileCharacterClass({ characters }: CharacterClass): string {
     return characters[0]!;
   }
 
-  return `[${escapeHypen(characters).join('')}]`;
+  return `[${escapeHyphen(characters).join('')}]`;
 }
 
-// If passed characters includes hypen (`-`) it need to be moved to
-// first (or last) place in order to treat it as hypen character and not a range.
+// If passed characters includes hyphen (`-`) it need to be moved to
+// first (or last) place in order to treat it as hyphen character and not a range.
 // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions/Character_classes#types
-function escapeHypen(characters: string[]) {
+function escapeHyphen(characters: string[]) {
   if (characters.includes('-')) {
     return ['-', ...characters.filter((c) => c !== '-')];
   }
