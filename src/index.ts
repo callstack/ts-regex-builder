942b7b8dcfad76053520eb0f7e68fd7f960d6014
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-12-26 17:55:12 +0100

    refactor: remove `one` component (#34)

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,18 @@ export type * from './types';
 
 export { buildPattern, buildRegex } from './builders';
 
+export { startOfString, endOfString } from './components/anchors';
 export { capture } from './components/capture';
 export {
   any,
-  anyOf,
   digit,
   whitespace,
   word,
+  anyOf,
+  characterRange,
+  characterClass,
+  inverted,
 } from './components/character-class';
 export { choiceOf } from './components/choice-of';
-export {
-  one,
-  oneOrMore,
-  optionally,
-  zeroOrMore,
-} from './components/quantifiers';
+export { oneOrMore, optionally, zeroOrMore } from './components/quantifiers';
 export { repeat } from './components/repeat';
