df04542ff050d9a977a1f149213f98d6583714ce
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2023-12-26 17:50:30 +0100

    refactor: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ export type * from './types';
 
 export { buildPattern, buildRegex } from './builders';
 
+export { startOfString, endOfString } from './components/anchors';
 export { capture } from './components/capture';
 export {
   any,
-  anyOf,
   digit,
   whitespace,
   word,
+  anyOf,
+  characterRange,
+  characterClass,
+  inverted,
 } from './components/character-class';
 export { choiceOf } from './components/choice-of';
 export { oneOrMore, optionally, zeroOrMore } from './components/quantifiers';
