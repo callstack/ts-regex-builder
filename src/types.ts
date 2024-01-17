ed0b63049a55110d8e092198f00378f955211ee8
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-01-17 23:48:12 +0000

    docs: ts-docs for types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,18 @@
 import type { EncodeOutput } from './encoder/types';
 
+/**
+ * Sequence of `RegexElements` that can be encoded into a regular expression.
+ */
 export type RegexSequence = RegexElement[] | RegexElement;
 
+/**
+ * Represents a result of calling a regex component (`RegexEncodable`) or a string to be matched literally.
+ */
 export type RegexElement = RegexEncodable | string;
 
+/**
+ * Represents result of calling a regex componen.
+ */
 export interface RegexEncodable {
   type: string;
   encode(): EncodeOutput;
