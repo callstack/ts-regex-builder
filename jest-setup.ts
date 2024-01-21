9aa0b5f73a365e095bc57133f60efde990431603
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-01-21 23:53:24 +0000

    refactor: cleanup

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,4 +1,4 @@
-import './test-utils/to-have-pattern';
+import './test-utils/to-equal-regex';
 import './test-utils/to-match-groups';
 import './test-utils/to-match-all-groups';
 import './test-utils/to-match-string';
