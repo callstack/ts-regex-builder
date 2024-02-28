c4a35db69673d43a3aef455fcfdb4e5940e8ffd0
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-28 21:44:38 +0100

    chore: fix docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://ts-regex-builder.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ts-regex-builder/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
