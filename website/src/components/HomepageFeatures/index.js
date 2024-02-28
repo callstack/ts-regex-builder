8fa18c564dc9fb6e4b8f7ac2c44867896392a7d1
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-28 21:55:12 +0100

    docs: update index

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -4,38 +4,28 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
+    title: 'Structured Regexes',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        TS Regex Builder lets you build maintainable regular expressions even for complex use cases.
       </>
     ),
   },
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: <>It's easy to learn, easy to read, and we have comprehensive docs too.</>,
+  },
+
+  {
+    title: 'Minimal Footprint',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: <>It weights only 1.8 kB when minified and gzipped. Supports tree shaking.</>,
+  },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
