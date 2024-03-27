39c8a1e44a673e09fd4369bb2e3bb8f36f8be759
PaulJPhilp <138239846+PaulJPhilp@users.noreply.github.com>
2024-03-27 14:56:23 -0400

    feat: adding IP addresses to the URL pattern

--- src/patterns/URL.ts
+++ src/patterns/URL.ts
@@ -13,6 +13,8 @@ import { choiceOf } from '../constructs/choice-of';
 import { repeat } from '../constructs/repeat';
 import { capture } from '../constructs/capture';
 import { oneOrMore, optional } from '../constructs/quantifiers';
+import { regex } from '../constructs/regex';
+
 
 //
 // The building blocks of the URL regex.
@@ -35,6 +37,80 @@ const usernameChars = charClass(lowercase, digit, specialChars);
 const hostnameChars = charClass(charRange('a', 'z'));
 const pathSpecialChars = anyOf(':@%._+~#=');
 const queryDelimiter = anyOf('&;');
+const hexDigit = choiceOf(charRange("0", "9"), charRange("a", "f"), charRange("A", "F"))
+const ipV6Group = repeat(hexDigit, { min: 1, max: 4, greedy: false })
+const ipV4Seperator = '.';
+const ipV6Seperator = ':';
+
+export const ipDigit = choiceOf(
+  regex(["2", "5", charRange("0", "5")]),
+  regex(["2", charRange("0", "4"), charRange("0", "9")]),
+  regex([anyOf("01"), charRange("0", "9"), charRange("0", "9")])
+)
+
+export const ipv4Address = regex([
+  ipDigit,
+  ipV4Seperator,
+  ipDigit,
+  ipV4Seperator,
+  ipDigit,
+  ipV4Seperator,
+  ipDigit
+])
+
+export const urlIpv4Finder = buildRegExp([capture(ipv4Address)], {
+  ignoreCase: true,
+  global: true,
+});
+
+export const urlIpv4Validator = buildRegExp([startOfString, capture(ipv4Address), endOfString], {
+  ignoreCase: true,
+});
+
+/***
+ *** IPv6 Address
+ *** Source: https://www.rfc-editor.org/rfc/rfc4291.html
+ ***
+ *** Form 1:
+ ***    ABCD:EF01:2345:6789:ABCD:EF01:2345:6789
+ ***    2001:DB8:0:0:8:800:200C:417A (collapsed leading zeros)
+ ***
+ *** Form 2:
+ ***    The following valid addresses:
+ ***      2001:DB8:0:0:8:800:200C:417A   a unicast address
+ ***      FF01:0:0:0:0:0:0:101           a multicast address
+ ***      0:0:0:0:0:0:0:1                the loopback address
+ ***      0:0:0:0:0:0:0:0                the unspecified address
+ ***
+ ***    Can be represented as the following collapsed valid addresses:
+ ***      2001:DB8::8:800:200C:417A      a unicast address
+ ***      FF01::101                      a multicast address
+ ***      ::1                            the loopback address
+ ***      ::                             the unspecified address
+ ***/
+
+export const ipv6Address = regex([
+  optional(ipV6Group),
+  optional(ipV6Seperator),
+  optional(ipV6Group),
+  optional(ipV6Seperator),
+  optional(ipV6Group),
+  optional(ipV6Seperator),
+  optional(ipV6Group),
+  ipV6Seperator,
+  optional(ipV6Group),
+  ipV6Seperator,
+  optional(ipV6Group),
+])
+
+export const urlIpv6Finder = buildRegExp([capture(ipv6Address)], {
+  ignoreCase: true,
+  global: true,
+});
+
+export const urlIpv6Validator = buildRegExp([startOfString, capture(ipv6Address), endOfString], {
+  ignoreCase: true,
+});
 
 //
 // Combining these building blocks into the following URL regex components:
@@ -93,7 +169,7 @@ export const urlAuthorityValidator = buildRegExp(
 //        A hostname (e.g. www.google.com)
 //
 
-export const urlHost = choiceOf(hostname);
+export const urlHost = choiceOf(hostname, ipv4Address, ipv6Address);
 
 export const urlHostFinder = buildRegExp(hostname, {
   ignoreCase: true,
