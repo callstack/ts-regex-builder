9c68cca23468c43c99f86ca55cbc92efb12a1bbc
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-03-27 16:01:52 +0100

    chore: fix tests

--- src/constructs/__tests__/regex.test.tsx
+++ src/constructs/__tests__/regex.test.tsx
@@ -5,9 +5,3 @@ test('`regex` no-op pattern', () => {
   expect(regex(['a', 'b'])).toEqualRegex(/ab/);
   expect([regex('a'), regex(['b', 'c'])]).toEqualRegex(/abc/);
 });
-
-test('`regex` throws on empty children', () => {
-  expect(() => regex([])).toThrowErrorMatchingInlineSnapshot(
-    `"\`regex\` should receive at least one element"`,
-  );
-});
