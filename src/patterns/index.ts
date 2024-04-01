68f1959583847ca6fd33ce93bf5026bf44524d2f
PaulJPhilp <138239846+PaulJPhilp@users.noreply.github.com>
2024-04-01 12:42:07 -0400

    feat:  added URL pattern to the patterns/index.ts file

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,4 +1,27 @@
 export { hexColorFinder, hexColorValidator } from './hex-color';
-export { urlFinder, urlValidator } from './url';
+export {
+  url,
+  urlFinder,
+  urlValidator,
+  urlHost,
+  urlHostFinder,
+  urlHostValidator,
+  urlScheme,
+  urlSchemeFinder,
+  urlSchemeValidator,
+  urlAuthority,
+  urlAuthorityFinder,
+  urlAuthorityValidator,
+  urlPath,
+  urlPathFinder,
+  urlPathValidator,
+  urlQuery,
+  urlQueryFinder,
+  urlQueryValidator,
+  urlFragment,
+  urlFragmentFinder,
+  urlFragmentValidator,
+} from './url';
+
 export { ipv4DigitValidator, ipv4Finder, ipv4Validator } from './ip-addr';
 export { ipv6Finder, ipv6Validator, ipv6GroupValidator } from './ip-addr';
