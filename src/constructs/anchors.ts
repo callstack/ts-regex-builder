e0aaddd05ab6f4f28aecaee6dd7c7967f9f65323
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-04-30 16:17:04 +0200

    refactor: simplify anchors

--- src/constructs/anchors.ts
+++ src/constructs/anchors.ts
@@ -1,43 +1,26 @@
 import type { EncodeResult } from '../encoder/types';
-import type { RegexConstruct } from '../types';
 
-export interface Anchor extends RegexConstruct {
-  type: 'anchor';
-  symbol: string;
-}
-
-export const startOfString: Anchor = {
-  type: 'anchor',
-  symbol: '^',
-  encode: encodeAnchor,
+export const startOfString: EncodeResult = {
+  precedence: 'atom',
+  pattern: '^',
 };
 
-export const endOfString: Anchor = {
-  type: 'anchor',
-  symbol: '$',
-  encode: encodeAnchor,
+export const endOfString: EncodeResult = {
+  precedence: 'atom',
+  pattern: '$',
 };
 
-export const wordBoundary: Anchor = {
-  type: 'anchor',
-  symbol: '\\b',
-  encode: encodeAnchor,
+export const wordBoundary: EncodeResult = {
+  precedence: 'atom',
+  pattern: '\\b',
 };
 
-export const nonWordBoundary: Anchor = {
-  type: 'anchor',
-  symbol: '\\B',
-  encode: encodeAnchor,
+export const nonWordBoundary: EncodeResult = {
+  precedence: 'atom',
+  pattern: '\\B',
 };
 
 /**
  * @deprecated Renamed to `nonWordBoundary`.
  */
 export const notWordBoundary = nonWordBoundary;
-
-function encodeAnchor(this: Anchor): EncodeResult {
-  return {
-    precedence: 'sequence',
-    pattern: this.symbol,
-  };
-}
