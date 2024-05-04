543f3baa1da5b2483707cdd199542e68e2e44bc7
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-05-05 00:03:37 +0200

    chore: proper ESM build with Babel (#90)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+
+  env: {
+    cjs: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }]],
+      plugins: [
+        ['@babel/plugin-transform-modules-commonjs', { strict: true }],
+        ['babel-plugin-add-import-extension', { extension: 'js' }],
+      ],
+      ignore: ['**/__tests__/*', '**/test.ts', '**/*.d.ts'],
+    },
+
+    esm: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' }, modules: false }]],
+      plugins: [['babel-plugin-add-import-extension', { extension: 'mjs' }]],
+      ignore: ['**/__tests__/*', '**/test.ts', '**/*.d.ts'],
+    },
+  },
 };
