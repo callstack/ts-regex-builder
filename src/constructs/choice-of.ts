147adbd8b1e1aa5cac9c6f07c01b7af6ad2bb08e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-05-06 09:29:07 +0200

    refactor: refactor choiceOf

--- src/constructs/choice-of.ts
+++ src/constructs/choice-of.ts
@@ -1,27 +1,13 @@
 import { encodeSequence } from '../encoder/encoder';
 import type { EncodedRegex } from '../encoder/types';
-import { ensureArray } from '../utils/elements';
-import type { RegexConstruct, RegexElement, RegexSequence } from '../types';
+import type { RegexSequence } from '../types';
 
-export interface ChoiceOf extends RegexConstruct {
-  type: 'choiceOf';
-  alternatives: RegexElement[][];
-}
-
-export function choiceOf(...alternatives: RegexSequence[]): ChoiceOf {
+export function choiceOf(...alternatives: RegexSequence[]): EncodedRegex {
   if (alternatives.length === 0) {
     throw new Error('`choiceOf` should receive at least one alternative');
   }
 
-  return {
-    type: 'choiceOf',
-    alternatives: alternatives.map((c) => ensureArray(c)),
-    encode: encodeChoiceOf,
-  };
-}
-
-function encodeChoiceOf(this: ChoiceOf): EncodedRegex {
-  const encodedAlternatives = this.alternatives.map((c) => encodeSequence(c));
+  const encodedAlternatives = alternatives.map((c) => encodeSequence(c));
   if (encodedAlternatives.length === 1) {
     return encodedAlternatives[0]!;
   }
