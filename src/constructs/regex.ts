d320f26859b840b3e63bb687e914e4af2c070588
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-05-06 09:26:15 +0200

    refactor: migrate regex

--- src/constructs/regex.ts
+++ src/constructs/regex.ts
@@ -1,21 +1,7 @@
 import { encodeSequence } from '../encoder/encoder';
 import type { EncodedRegex } from '../encoder/types';
-import { ensureArray } from '../utils/elements';
-import type { RegexConstruct, RegexElement, RegexSequence } from '../types';
+import type { RegexSequence } from '../types';
 
-export interface Regex extends RegexConstruct {
-  type: 'sequence';
-  children: RegexElement[];
-}
-
-export function regex(sequence: RegexSequence): Regex {
-  return {
-    type: 'sequence',
-    children: ensureArray(sequence),
-    encode: encodeRegex,
-  };
-}
-
-function encodeRegex(this: Regex): EncodedRegex {
-  return encodeSequence(this.children);
+export function regex(sequence: RegexSequence): EncodedRegex {
+  return encodeSequence(sequence);
 }
